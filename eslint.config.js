7d9dfbdb0155737a79721c5568e4aec776bd4920
Syomei Aoshima <neko@asyomei.org>
2025-05-11 22:05:37 +0500

    chore(eslint): disable some rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,9 @@ export default antfu({
   astro: true,
   typescript: true,
   unocss: true,
+  rules: {
+    'antfu/if-newline': 'off',
+    'style/brace-style': 'off',
+    'node/prefer-global/process': 'off',
+  },
 })
